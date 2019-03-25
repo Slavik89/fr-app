5b5c1356904178dbe276c14f1e01108ec553313c
Slavik89 <terakrokos@gmail.com>
2019-03-25 20:50:30 +0200

    Minor fixing

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import logo from './../logo.svg';
 function Home () {
     return (
       <div className="home-container">        
-        <h1>Now I'm learning React</h1>
+        <p>Hi, I'm learning React now</p>
         <img src={logo} className="App-logo" alt="logo" />
       </div>
     )
